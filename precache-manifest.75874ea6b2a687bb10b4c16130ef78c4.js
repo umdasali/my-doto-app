05ab1d1e05a358693804f6912a6953500f434d9c
MD ALI SHER ALI <67758260+umdasali@users.noreply.github.com>
2020-08-21 19:40:52 +0530

    Updates

---
+++ precache-manifest.75874ea6b2a687bb10b4c16130ef78c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f696cdb44ec01a8835da4d4e17999875",
+    "url": "/my-doto-app/index.html"
+  },
+  {
+    "revision": "7f9fa2e51e2bfeadc27c",
+    "url": "/my-doto-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "52ad13579c6aa005ed77",
+    "url": "/my-doto-app/static/js/2.e6b61271.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-doto-app/static/js/2.e6b61271.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f9fa2e51e2bfeadc27c",
+    "url": "/my-doto-app/static/js/main.97687b1c.chunk.js"
+  },
+  {
+    "revision": "592f28b6be01445ab468",
+    "url": "/my-doto-app/static/js/runtime-main.3fc8c690.js"
+  }
+]);
