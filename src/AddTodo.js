085caa0ebe33232ae275e8df816eea6642b21d1a
MD ALI SHER ALI <67758260+umdasali@users.noreply.github.com>
2020-08-21 19:23:42 +0530

    Test react App

---
+++ src/AddTodo.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+class AddTodo extends React.Component{
+    state={
+        content:''
+    }
+   handleChange = (e) =>{
+       this.setState({
+           content: e.target.value
+       })
+   }
+
+   handleSubmit = (e) => {
+       e.preventDefault();
+       this.props.addTodo(this.state);
+       this.setState({
+         content:''
+       })
+   }
+
+    render(){
+      return(
+        <div>
+        <form onSubmit={this.handleSubmit}>
+        <label>AddTodo</label>
+        <input type="text" onChange={this.handleChange} value={this.state.content}/>
+        </form>
+        </div>
+      )
+    }
+}
+export default AddTodo
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Todos from './Todos'
+import AddTodo from './AddTodo'
+
+
+class App extends React.Component {
+    state = {
+        todos:[
+            {id: 1, content:'buy some milk'},
+            {id: 2, content:'play ml'},
+            {id: 3, content:'buy some extra milk'}
+        ]
+    }
+
+deleteTodo=(id)=>{
+    const todos = this.state.todos.filter(todo =>{
+        return todo.id !== id
+    });
+    this.setState({
+        todos
+    })
+}
+    addTodo = (todo) =>{
+      todo.id = Math.random();
+      let todos = [...this.state.todos, todo]
+      this.setState({
+        todos
+      })
+    }
+    render() {
+        return (
+            <div className="App container">
+             <h1 className="center blue-text">Todo's</h1>
+            <Todos todos = {this.state.todos} deleteTodo={this.deleteTodo}/>
+            <AddTodo addTodo={this.addTodo}/>
+            </div>
+        );
+    }
+}
+export default App;
